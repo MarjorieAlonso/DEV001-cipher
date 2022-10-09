7d61936e7bdf8a2ffee44ef0dec098832b00d732
MarjorieAlonso <114000910+MarjorieAlonso@users.noreply.github.com>
2022-10-09 11:18:58 -0300

    avance

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,47 @@
 const cipher = {
-  // ...
+  encode: function (mensaje, offset) {
+    let result = "";
+    let code;
+    for (let i = 0; i < mensaje.length; i++) {
+
+      if (mensaje.charCodeAt(i) >= 65 && mensaje.charCodeAt(i) <= 90) {
+        code = (((mensaje.charCodeAt(i) - 65) + offset) % 26) + 65;
+
+      }
+      else if (mensaje.charCodeAt(i) >= 97 && mensaje.charCodeAt(i) <= 122) {
+        code = (((mensaje.charCodeAt(i) - 97) + offset) % 26) + 97;
+      }
+      else if (mensaje.charCodeAt(i) === 32) {
+        code = 32;
+      }
+
+      result += String.fromCharCode(code)
+
+    }
+
+    return result;
+
+  },
+
 };
+const decodificar = {
+  decode: function (message, offset) {
+    let result = "";
+    let codigo;
+    for (let i = 0; i < string.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        codigo = (((message.charCodeAt(i) - 65) - offset) % 26) + 65;
+      }
+      else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        codigo = (((message.charCodeAt(i) - 97) - offset) % 26) + 97;
+      }
+      else if (message.charCodeAt(i) === 32) {
+        codigo = 32;
+      }
+      result += String.fromCharCode(codigo);
+    }
+    return result;
+  };
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
+//  funcion boton de codificar
+let button = document.getElementById("codificar");
+button.addEventListener("click", function () {
+
+    //constante primera caja 
+
+    let mensaje = document.getElementById("mensaje1").value;
+    //boton offsett
+    const offset = parseInt(document.getElementById("offset").value);
+    document.getElementById("mensaje1").value = cipher.encode(mensaje, offset);
+    //console.log(mensaje);
+
+})
+//funcion decodificar
+document.getElementById("mensaje2").addEventListener("click", function () {
+    let message = document.getElementById("mensaje2").value;
+    //console.log("mensaje2");
+    //boton de decodificar
+    let boton = parseInt(document.getElementById("decodificar").value);
+    boton.addEventListener("click", function () {
+        const offset = parseInt(document.getElementById("offset").value);
+        document.getElementById("mensaje2").valor = cipher.decodificar(message, offset);
+    })
+}
+)
 console.log(cipher);
